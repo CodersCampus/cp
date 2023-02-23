0a71444b14f123ec3c6a65f55cf7a1e8d2bcf14c
Tyler Miceli <tymiceli24@gmail.com>
2023-02-22 21:07:27 -0500

    did crud on dog

---
+++ src/main/java/com/coderscampus/springwise/service/DogService.java
@@ -0,0 +1,26 @@
+package com.coderscampus.springwise.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.coderscampus.springwise.domain.Dog;
+import com.coderscampus.springwise.repository.DogRepository;
+
+@Service
+public class DogService {
+	
+	@Autowired
+	private DogRepository dogRepo;
+	
+	public Dog saveDog(Dog dog) {
+		return dogRepo.save(dog);
+	}
+
+	public List<Dog> findAll() {
+
+		return dogRepo.findAll();
+	}
+
+}
---
+++ src/main/java/com/coderscampus/springwise/web/DogController.java
@@ -0,0 +1,43 @@
+package com.coderscampus.springwise.web;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.coderscampus.springwise.domain.Dog;
+import com.coderscampus.springwise.service.DogService;
+
+@Controller
+public class DogController {
+	
+	@Autowired
+	private DogService dogService;
+	
+	@GetMapping("/dog")
+	public String getHome (ModelMap model) {
+		
+		Dog dog = new Dog();
+		
+		model.put("dog", dog);
+		
+		return "createdog";
+	}
+
+	@PostMapping("/save-dog")
+	public String saveDog(Dog dog) {
+		dogService.saveDog(dog);
+		return "redirect:/dogs";
+	}
+	
+	@GetMapping("/dogs")
+	public String getDogPage (ModelMap model) {
+		List<Dog> dogs = dogService.findAll();
+		
+		model.put("dogs", dogs);
+		return "dogs";
+	}
+}
