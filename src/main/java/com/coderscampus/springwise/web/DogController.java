1f2c21149dd11e97f35031332888aa422845cfd8
Tyler Miceli <tymiceli24@gmail.com>
2023-03-01 20:36:37 -0500

    merged dev to dog branch

--- src/main/java/com/coderscampus/springwise/web/DogController.java
+++ src/main/java/com/coderscampus/springwise/web/DogController.java
@@ -7,17 +7,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.coderscampus.springwise.domain.Dog;
 import com.coderscampus.springwise.service.DogService;
 
 @Controller
+@RequestMapping("/dog")
 public class DogController {
 	
 	@Autowired
 	private DogService dogService;
 	
-	@GetMapping("/dog")
+	@GetMapping("/create")
 	public String getHome (ModelMap model) {
 		
 		Dog dog = new Dog();
