b9a10c7b62ae39d4e20371b79ecde062d2f2ca87
Joseph Kirkish <kirkish.2@gmail.com>
2023-05-01 20:59:52 -0400

    Airplane views
    airplane object added for Crud operations

---
+++ src/main/java/com/coderscampus/springwise/web/AirplaneController.java
@@ -0,0 +1,68 @@
+package com.coderscampus.springwise.web;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.coderscampus.springwise.domain.Airplane;
+import com.coderscampus.springwise.service.AirplaneService;
+
+@Controller
+@RequestMapping("/airplane")
+public class AirplaneController {
+	
+	@Autowired
+	private AirplaneService airplaneService;
+	
+	@GetMapping("/")
+	public String home(ModelMap model) {
+		List<Airplane> airplanes = airplaneService.findAll();
+		model.put("airplanes", airplanes);
+		return "airplane/read";
+	}
+	
+	@GetMapping("/create")
+	public String getCreate (ModelMap model) {
+		Airplane airplane = new Airplane();
+		model.put("airplane", airplane);
+		return "airplane/create";
+	}
+
+	@PostMapping("/create")
+	public String create(Airplane airplane) {
+		airplaneService.save(airplane);
+		return "redirect:/airplane/";
+	}
+// This is same mapping as Airplane Home page-->left here just in case.(may need deleted)
+//	@GetMapping("/read")
+//	public String read(ModelMap model) {
+//		List<Airplane> airplanes = airplaneService.findAll();
+//		model.put("airplanes", airplanes);
+//		return "airplane/read";
+//	}
+	
+	@GetMapping("/update/{id}")
+	public String fetch(ModelMap model, @PathVariable Long id) {
+		Airplane airplane = airplaneService.findById(id);
+		model.put("airplane", airplane);
+		return "airplane/update";
+	}
+	
+	@PostMapping("/update")
+	public String update(Airplane airplane) {
+		airplaneService.save(airplane);
+		return "redirect:/airplane/";
+	}
+	
+	@PostMapping("/delete")
+	public String delete(Airplane airplane) {
+		airplaneService.delete(airplane);
+		return "redirect:/airplane/";
+	}
+}
