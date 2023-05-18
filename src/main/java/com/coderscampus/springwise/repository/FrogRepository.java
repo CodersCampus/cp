1932ce5636d1bb16d787cf302f559a520150ad2d
Asma Farooq <asma.faarook@gmail.com>
2023-05-17 17:44:39 -0700

    Commit
    Created FrogService, FrogController, htmls pages

--- src/main/java/com/coderscampus/springwise/repository/FrogRepository.java
+++ src/main/java/com/coderscampus/springwise/repository/FrogRepository.java
@@ -1,9 +1,11 @@
 package com.coderscampus.springwise.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.coderscampus.springwise.domain.Frog;
 
+@Repository
 public interface FrogRepository extends JpaRepository<Frog, Long>{
 
 }
