78eb809f946e4e6bad773a9aed7eef1c82443427
Joseph Kirkish <kirkish.2@gmail.com>
2023-05-31 20:59:52 -0400

    refactored methods

--- src/main/java/com/coderscampus/springwise/service/SeedService.java
+++ src/main/java/com/coderscampus/springwise/service/SeedService.java
@@ -32,45 +32,31 @@ public class SeedService {
 	private DataFileService dataFileService;
 	@Autowired
 	private TruckRepository truckRepo;
+	
+	private Random random = new Random();
 
-	public Map<Long, Car> carCreation() {
-
-		Random random = new Random();
-		String[] motorSizes = { "Small", "Medium", "Large" };
-		String[] modelNames = { "Audi", "Lamborghini", "BMW", "Volkswagen", "Kia", "Honda", "Chevrolet", "Ford",
-				"Lincoln", "LandRover", "Infiniti", "Nissan", "Ram", "Toyota", "Tesla", "Subaru", "Volvo" };
-
-		Integer[] wheelSizes = { 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
-		String[] transmissions = { "automatic", "manual", "hybrid" };
-		String[] colors = { "Yellow", "Red", "Blue", "Orange", "Purple", "White", "Black", "Grey" };
-		Integer[] years = { 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022 };
-		BigDecimal[] prices = { new BigDecimal(29845), new BigDecimal(58630), new BigDecimal(75392) };
-
-		Long i = 1L;
-		while (i <= 10) {
-			String motorSize = motorSizes[random.nextInt(motorSizes.length - 1)];
-			String modelName = modelNames[random.nextInt(modelNames.length - 1)];
-			Integer wheelSize = wheelSizes[random.nextInt(wheelSizes.length - 1)];
-			String transmission = transmissions[random.nextInt(transmissions.length - 1)];
-			String color = colors[random.nextInt(colors.length - 1)];
-			Integer year = years[random.nextInt(years.length - 1)];
-			BigDecimal price = prices[random.nextInt(prices.length - 1)];
-			cars.put(i, new Car(i, motorSize, modelName, wheelSize, transmission, color, year, price));
-			i++;
-		}
-		return cars;
-
-	}
+	
 
 	public void populateData() {
 
-		Random random = new Random();
+		
+		seedDataAirplanes();
+
+		seedDataFrogs();
+
+		seedDataDogs();
+		seedDataTrucks();
+	}
+
+	private void seedDataAirplanes() {
 		Airplane airplane = new Airplane();
 		airplane.setModel("747");
 		airplane.setMake("Boeing");
 		airplane.setColor("Fusia");
 		airRepo.save(airplane);
+	}
 
+	private void seedDataFrogs() {
 		String[] frogName = {"kermit", "frogger", "mr. toad", "tree frog"};
 		String[] frogSpecies = {"blue", "green", "pink", "purple"};
 		String[] frogAge = {"young", "old", "young", "old"};
@@ -86,7 +72,9 @@ public class SeedService {
 			frogRepo.save(frog);
 
 		}
+	}
 
+	private void seedDataDogs() {
 		String[] dogName = {"Coco", "Woof", "Shrimp", "Nuts", "Rover", "Wilber"};
 		String[] dogBreed = {"Shepperd", "Chihuahua", "Pomeranian", "Poodle"};
 		String[] dogAge = {"young", "old", "young", "old"};
@@ -102,6 +90,10 @@ public class SeedService {
 			dogRepo.save(dog);
 
 		}
+	}
+
+	private void seedDataTrucks() {
+		
 		List<Truck> trucks = truckRepo.findAll();
 		if (trucks.size() < 10) {
 			String[] trucksData = dataFileService.getData("src/main/resources/data/trucks.txt");
