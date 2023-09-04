488fb9bc5091f2e12a7f1241f8bc26957a9ce156
Pete Carapetyan <pete.carapetyan@gmail.com>
2023-09-04 17:11:50 -0500

    fixed CORS error

--- src/main/java/com/coderscampus/springwise/web/XmplDataController.java
+++ src/main/java/com/coderscampus/springwise/web/XmplDataController.java
@@ -2,7 +2,8 @@ package com.coderscampus.springwise.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,18 +13,19 @@ import com.coderscampus.springwise.service.XmplDataService;
 
 @RestController
 @RequestMapping("/xmpl")
+@CrossOrigin(origins = "*")
 public class XmplDataController {
 
 	@Autowired
 	private XmplDataService xmplDataService;
 
-	@GetMapping("/data")
+	@PostMapping("/data")
 	public ResponseEntity<XmplData> getData() {
 		XmplData xmplData =  xmplDataService.getData();
 		return ResponseEntity.ok(xmplData);
 	}
 
-	@GetMapping("/datum")
+	@PostMapping("/datum")
 	public ResponseEntity<XmplDatum> getDatum() {
 		XmplDatum xmplDatum =  xmplDataService.getDatum();
 		return ResponseEntity.ok(xmplDatum);
