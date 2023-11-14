b5e738aefa330173473cbf08e4135ed810022275
MiriamLaw <mimsical999@yahoo.com>
2023-11-13 18:00:32 -0800

    Still not working
    Moved everything to practice packages and not finding different classes in our new package, probably has to do with componentscan

--- src/main/java/com/coderscampus/springwise/SpringWiseApplication.java
+++ src/main/java/com/coderscampus/springwise/SpringWiseApplication.java
@@ -2,8 +2,11 @@ package com.coderscampus.springwise;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.coderscampus.practice", "com.coderscampus.springwise"})
+
 public class SpringWiseApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringWiseApplication.class, args);
