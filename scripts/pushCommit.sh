d7c0ed1db456e81bc2b510f0ca0a6eddb97cfece
Pete Carapetyan <pete.carapetyan@gmail.com>
2023-11-30 17:02:42 -0600

    Second commit, this will work if I did it right

--- scripts/pushCommit.sh
+++ scripts/pushCommit.sh
@@ -6,13 +6,24 @@ fi
 
 # Set the internal variable 'comment' to the provided argument
 comment="$1"
+# Function to check if the current branch is "dev"
+is_dev_branch() {
+    current_branch=$(git symbolic-ref --short HEAD 2>/dev/null)
+    [ "$current_branch" = "dev" ]
+}
 
 # Check the length of 'comment' and echo accordingly
 if [ ${#comment} -gt 2 ]; then
     echo "Pushing commit with comment: $comment"
     git add .
     git commit -m "$comment"
-    git push
+    if is_dev_branch; then
+        echo "The 'dev' branch is currently checked out. Aborting git push."
+        exit 1
+    else
+        echo "The current branch is not 'dev'. Proceeding with git push."
+        git push
+    fi
 else
     echo "Sorry dude"
 fi
