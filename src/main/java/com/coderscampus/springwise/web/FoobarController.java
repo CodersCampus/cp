ad35caac7226ef14c7831ea3ff7a22ce0bccc4a4
Dave Naugler <davenaugler@gmail.com>
2024-01-23 12:43:18 -0700

    Added to sysout in FoobarController.java

--- src/main/java/com/coderscampus/springwise/web/FoobarController.java
+++ src/main/java/com/coderscampus/springwise/web/FoobarController.java
@@ -39,7 +39,7 @@ public class FoobarController {
 		System.out.println("in/create" + foobar + uid);
 		foobar = foobarService.saveByUid(foobar, uid);
 // coming back to this after issue329, we're making a separate issue.
-		System.out.println("FOOBAR" + foobar);
+		System.out.println("FOOBAR: " + foobar);
 		return "redirect:/foobar/";
 	}
 
