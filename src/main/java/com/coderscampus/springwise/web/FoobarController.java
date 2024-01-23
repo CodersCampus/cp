7211d89213bb037c0b35ab2a9f7b27d4b908f9f5
Karen Dillehay <117693236+Kadillehay@users.noreply.github.com>
2024-01-22 19:39:51 -0600

    ran into issue with foobar controller/uid. creating separate issue to tackle that

--- src/main/java/com/coderscampus/springwise/web/FoobarController.java
+++ src/main/java/com/coderscampus/springwise/web/FoobarController.java
@@ -12,50 +12,51 @@ import java.util.List;
 @Controller
 @RequestMapping("/foobar")
 public class FoobarController {
-	
+
 	@Autowired
 	private FoobarService foobarService;
-	
+
 	@GetMapping("/")
 	public String home(ModelMap model) {
 		List<Foobar> foobars = foobarService.findAll();
 		model.put("foobars", foobars);
-        model.addAttribute("pageTitle", "Foobar Read");
+		model.addAttribute("pageTitle", "Foobar Read");
 		model.put("isFoobar", true);
 		return "foobar/read";
 	}
-	
+
 	@GetMapping("/create")
-	public String getCreate (ModelMap model) {
+	public String getCreate(ModelMap model) {
 		Foobar foobar = new Foobar();
 		model.put("foobar", foobar);
-        model.addAttribute("pageTitle", "Foobar Create");
+		model.addAttribute("pageTitle", "Foobar Create");
 		model.put("isFoobar", true);
 		return "foobar/create";
 	}
+
 	@PostMapping("/create")
 	public String create(Foobar foobar, @RequestParam("uid") String uid) {
 		System.out.println("in/create" + foobar + uid);
-		foobarService.save(foobar); 
+//		foobarService.save(foobar); 
+// coming back to this after issue329, we're making a separate issue.
 		return "redirect:/foobar/";
 	}
 
-	
 	@GetMapping("/update/{id}")
 	public String fetch(ModelMap model, @PathVariable Long id) {
 		Foobar foobar = foobarService.findById(id);
 		model.put("foobar", foobar);
-        model.addAttribute("pageTitle", "Foobar Update");
+		model.addAttribute("pageTitle", "Foobar Update");
 		model.put("isFoobar", true);
 		return "foobar/update";
 	}
-	
+
 	@PostMapping("/update")
 	public String update(Foobar foobar) {
 		foobarService.save(foobar);
 		return "redirect:/foobar/";
 	}
-	
+
 	@PostMapping("/delete")
 	public String delete(Foobar foobar) {
 		foobarService.delete(foobar);
