ba95f63cb5fa800ef8f678eae397f60564cde39e
Dave Naugler <davenaugler@gmail.com>
2024-02-12 18:03:42 -0700

    Cleaned up Student POJO

--- src/main/java/com/coderscampus/springwise/domain/Student.java
+++ src/main/java/com/coderscampus/springwise/domain/Student.java
@@ -12,6 +12,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
+import org.springframework.boot.actuate.endpoint.web.Link;
 
 @Entity
 public class Student {
@@ -41,6 +42,105 @@ public class Student {
 	private Website website;
 	@OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
 	private List<Checkin> checkin = new ArrayList<Checkin>();
+
+
+	public Student() {}
+
+	public Student(long id, String uid, String name, Integer assignmentNum) {
+		this.id = id;
+		this.uid = uid;
+		this.name = name;
+		this.assignmentNum = assignmentNum;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getUid() {
+		return uid;
+	}
+
+	public void setUid(String uid) {
+		this.uid = uid;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getAssignmentNum() {
+		return assignmentNum;
+	}
+
+	public void setAssignmentNum(Integer assignmentNum) {
+		this.assignmentNum = assignmentNum;
+	}
+
+	public GitHub getGithubHandle() {
+		return githubHandle;
+	}
+
+	public void setGithubHandle(GitHub githubHandle) {
+		this.githubHandle = githubHandle;
+	}
+
+	public LinkedIn getLinkedIn() {
+		return linkedIn;
+	}
+
+	public void setLinkedIn(LinkedIn linkedIn) {
+		this.linkedIn = linkedIn;
+	}
+
+	public String getIde() {
+		return ide;
+	}
+
+	public void setIde(String ide) {
+		this.ide = ide;
+	}
+
+	public YouTube getYoutube() {
+		return youtube;
+	}
+
+	public void setYoutube(YouTube youtube) {
+		this.youtube = youtube;
+	}
+
+	public FinalProject getFinalProject() {
+		return finalProject;
+	}
+
+	public void setFinalProject(FinalProject finalProject) {
+		this.finalProject = finalProject;
+	}
+
+	public Resume getResume() {
+		return resume;
+	}
+
+	public void setResume(Resume resume) {
+		this.resume = resume;
+	}
+
+	public Foobar getFoobar() {
+		return foobar;
+	}
+
+	public void setFoobar(Foobar foobar) {
+		this.foobar = foobar;
+	}
+
 	public Networking getNetworking() {
 		return networking;
 	}
@@ -49,89 +149,41 @@ public class Student {
 		this.networking = networking;
 	}
 
-	
-
-	public Student() {}
-
-
-
-	public Student(long id, String name, Integer assignmentNum, GitHub githubHandle, LinkedIn linkedIn, String ide,
-			String uid, YouTube youtube, FinalProject finalProject, Resume resume, Networking networking, Website website) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.assignmentNum = assignmentNum;
-		this.githubHandle = githubHandle;
-		this.linkedIn = linkedIn;
-		this.ide = ide;
-		this.uid = uid;
-		this.youtube = youtube;
-		this.finalProject = finalProject;
-		this.resume = resume;
-		this.networking = networking;
-		this.website = website;
-	}
-
-
 	public Website getWebsite() {
 		return website;
 	}
+
 	public void setWebsite(Website website) {
 		this.website = website;
 	}
-	public YouTube getYoutube() {
-		return youtube;
-	}
-	public void setYoutube(YouTube youtube) {
-		this.youtube = youtube;
-	}
-	public String getUid() {
-		return uid;
-	}
-	public void setUid(String uid) {
-		this.uid = uid;
-	}
-	public long getId() {
-		return id;
-	}
-	public void setId(long id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public Integer getAssignmentNum() {
-		return assignmentNum;
-	}
-	public void setAssignmentNum(Integer assignmentNum) {
-		this.assignmentNum = assignmentNum;
-	}
-	public GitHub getGithubHandle() {
-		return githubHandle;
-	}
-	public void setGithubHandle(GitHub githubHandle) {
-		this.githubHandle = githubHandle;
-	}
-	public String getIde() {
-		return ide;
-	}
-	public void setIde(String ide) {
-		this.ide = ide;
-	}
-	public LinkedIn getLinkedIn() {
-		return linkedIn;
-	}
-	public void setLinkedIn(LinkedIn linkedIn) {
-		this.linkedIn = linkedIn;
-	}
-	public FinalProject getFinalProject() { return finalProject; }
-	public void setFinalProject(FinalProject finalProject) { this.finalProject = finalProject; }
-	public Resume getResume() { return resume; }
-	public void setResume(Resume resume) { this.resume = resume; }
 
+	public List<Checkin> getCheckin() {
+		return checkin;
+	}
+
+	public void setCheckin(List<Checkin> checkin) {
+		this.checkin = checkin;
+	}
+
+
+	public Student(long id, String uid, String name, Integer assignmentNum, GitHub githubHandle, LinkedIn linkedIn, String ide, YouTube youtube,
+				   FinalProject finalProject, Resume resume, Foobar foobar, Networking networking, Website website, List<Checkin> checkin) {
+		super();
+		this.id = id;
+		this.uid = uid;
+		this.name = name;
+		this.assignmentNum = assignmentNum;
+		this.githubHandle = githubHandle;
+		this.linkedIn = linkedIn;
+		this.ide = ide;
+		this.youtube = youtube;
+		this.finalProject = finalProject;
+		this.resume = resume;
+		this.foobar = foobar;
+		this.networking = networking;
+		this.website = website;
+		this.checkin = checkin;
+	}
 
 	@Override
 	public String toString() {
@@ -149,6 +201,7 @@ public class Student {
 				", foobar=" + foobar +
 				", networking=" + networking +
 				", website=" + website +
+				", checkin=" + checkin +
 				'}';
 	}
 }
