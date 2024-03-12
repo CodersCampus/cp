0819157192a125d7b7d26632eefbbe10b30adfc7
JP-CC-Projects <joeparkcc@gmail.com>
2024-03-12 13:32:27 -0500

    added authdto and  http session structure to send dto back to thymeleaf

--- src/main/java/com/coderscampus/cp/web/SpringProjectController.java
+++ src/main/java/com/coderscampus/cp/web/SpringProjectController.java
@@ -2,7 +2,10 @@ package com.coderscampus.cp.web;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.coderscampus.cp.domain.Checkin;
+import com.coderscampus.cp.dto.AuthObjectDto;
+import com.coderscampus.cp.service.CheckinService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
@@ -12,24 +15,44 @@ import com.coderscampus.cp.domain.SpringProject;
 import com.coderscampus.cp.domain.Student;
 import com.coderscampus.cp.repository.SpringProjectRepository;
 import com.coderscampus.cp.service.StudentService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class SpringProjectController {
 
-    @Autowired
     private SpringProjectRepository springProjectRepository;
-    
-    @Autowired
     private StudentService studentService;
+    private CheckinService checkinService;
 
-    
+    public SpringProjectController(SpringProjectRepository springProjectRepository, StudentService studentService, CheckinService checkinService) {
+        this.springProjectRepository = springProjectRepository;
+        this.studentService = studentService;
+        this.checkinService = checkinService;
+    }
     @GetMapping("/")
-    public String getDashboard(ModelMap model) {
-    	Student student = new Student();
+    public String getDashboard(ModelMap model, HttpSession httpSession) {
+        String uid = (String)httpSession.getAttribute("authUid");
+        String displayName = (String)httpSession.getAttribute("displayName");
+        Checkin checkin = new Checkin();
+        checkin = checkinService.saveByUid(checkin, uid);
+        Student student = new Student();
 		model.put("student", student);
     	return "dashboard";
     } 
-    
+    @PostMapping("/send-oauth")
+    @ResponseBody
+    public String getOauth(@RequestBody AuthObjectDto authDto,
+                                      HttpSession httpSession) {
+        System.out.println(authDto);
+        if(authDto != null){
+            httpSession.setAttribute("authUid", authDto.getUid());
+            httpSession.setAttribute("displayName", authDto.getDisplayName());
+        }
+        return "redirect:/";
+    }
+
     @GetMapping("/springprojects")
     public String getSpringProjects(Model model) {
         List<SpringProject> projects = springProjectRepository.findAll();
