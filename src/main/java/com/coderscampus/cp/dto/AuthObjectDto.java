af4c11c51185671f0b9032cc543796926fc8b0fa
Dave Naugler <davenaugler@gmail.com>
2024-03-22 11:53:55 -0600

    studentName and photoURL are visable on all pages
    Needed to update the authscript.html to check to see if uid element exists before setting it's value on line 30. Also check if displayName element exists before setting it's value.

--- src/main/java/com/coderscampus/cp/dto/AuthObjectDto.java
+++ src/main/java/com/coderscampus/cp/dto/AuthObjectDto.java
@@ -3,7 +3,8 @@ package com.coderscampus.cp.dto;
 public class AuthObjectDto {
     private String uid;
     private String displayName;
-
+    //  DO NOT NEED FOR #512 TO WORK BUT NICE TO HAVE
+//    private String photoURL; // Add this field
 
     public String getUid() {
         return uid;
@@ -20,6 +21,15 @@ public class AuthObjectDto {
     public void setDisplayName(String displayName) {
         this.displayName = displayName;
     }
+
+    //  DO NOT NEED FOR #512 TO WORK BUT NICE TO HAVE
+//    public String getPhotoURL() {
+//        return photoURL;
+//    }
+//
+//    public void setPhotoURL(String photoURL) {
+//        this.photoURL = photoURL;
+//    }
 
     @Override
     public String toString() {
