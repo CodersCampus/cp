fbbec305d7aafb9fc1a0b2dce171c8767e5bbf10
olearyc11 <157394769+olearyc11@users.noreply.github.com>
2024-04-10 09:59:40 -0400

    Issue Work in Progress

--- src/main/java/com/coderscampus/cp/service/ActivityLogService.java
+++ src/main/java/com/coderscampus/cp/service/ActivityLogService.java
@@ -1,11 +1,35 @@
 package com.coderscampus.cp.service;
 
 import com.coderscampus.cp.domain.ActivityLog;
+import com.coderscampus.cp.domain.Checkin;
+import com.coderscampus.cp.repository.ActivityLogRepository;
+import com.coderscampus.cp.repository.CheckinRepository;
+
+import jakarta.transaction.Transactional;
+
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class ActivityLogService {
-    public void saveByUid(ActivityLog activityLog, String uid, String clientTimeZone) {
+	private final ActivityLogRepository activityLogRepository;
+	private final CheckinRepository checkinRepository;
+	public ActivityLogService(CheckinRepository checkinRepository, ActivityLogRepository activityLogRepository) {
+		super();
+		this.checkinRepository = checkinRepository;
+		this.activityLogRepository = activityLogRepository;
+	}
+	
+	@Transactional
+    public void saveByUid(ActivityLog activityLog, String uid) {
+		//Please examine objects here in DeBug
+		Checkin checkin = activityLog.getCheckin();
+		List<ActivityLog> activityLogList = checkin.getActivityLog();
+		activityLogList.add(activityLog);
+		checkinRepository.save(checkin);
+		activityLogRepository.save(activityLog);
+		
 //        setStudentAndUid(checkin, uid);
 //        setDateIfNull(checkin, clientTimeZone);
 //        return checkinRepo.save(checkin);
