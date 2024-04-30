93a2d3bd583fc65275bdeb1b8d3289d350754dae
K4ss0 <mikalandrs@gmail.com>
2024-04-29 17:46:39 -0700

    Fixed imports in student controller
    Fixed imports

--- src/main/java/com/coderscampus/cp/web/StudentController.java
+++ src/main/java/com/coderscampus/cp/web/StudentController.java
@@ -1,12 +1,18 @@
 package com.coderscampus.cp.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import com.coderscampus.cp.domain.Student;
 import com.coderscampus.cp.dto.StudentDTO;
 import com.coderscampus.cp.service.StudentService;
+
 import jakarta.servlet.http.HttpSession;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/student")
