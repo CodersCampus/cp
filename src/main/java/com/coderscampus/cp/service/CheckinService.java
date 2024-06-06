892e3ec7f297e7b15e1167f45030605af539e3d6
Abe885 <abrahamgc1901@gmail.com>
2024-06-06 11:32:34 -0700

    CheckinService test findByUid is working and checkinService saveByUid updated

--- src/main/java/com/coderscampus/cp/service/CheckinService.java
+++ src/main/java/com/coderscampus/cp/service/CheckinService.java
@@ -1,76 +1,77 @@
 package com.coderscampus.cp.service;
 
-import java.util.ArrayList;
+import com.coderscampus.cp.domain.Checkin;
+import com.coderscampus.cp.domain.Student;
+import com.coderscampus.cp.dto.CheckinDTO;
+import com.coderscampus.cp.repository.CheckinRepository;
+import com.coderscampus.cp.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import com.coderscampus.cp.dto.CheckinDTO;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.actuate.autoconfigure.info.InfoEndpointAutoConfiguration;
-import org.springframework.stereotype.Service;
-
-import com.coderscampus.cp.domain.Checkin;
-import com.coderscampus.cp.domain.Student;
-import com.coderscampus.cp.repository.CheckinRepository;
-import com.coderscampus.cp.repository.StudentRepository;
-
 @Service
 public class CheckinService {
 
-	@Autowired
-	private CheckinRepository checkinRepo;
+    @Autowired
+    private CheckinRepository checkinRepo;
 
-	@Autowired
-	private StudentRepository studentRepo;
+    @Autowired
+    private StudentRepository studentRepo;
 
-	public CheckinDTO saveByUid(CheckinDTO checkinDTO, String uid) {
-		Checkin foundCheckin = checkinRepo.findById(checkinDTO.getId()).orElse(null);
-		if (foundCheckin == null) {
-			Checkin checkin = new Checkin(checkinDTO, uid);
-			setStudentFromUid(checkin, uid);
-			foundCheckin = checkinRepo.save(checkin);
+    public CheckinDTO saveByUid(CheckinDTO checkinDTO, String uid) {
+        Checkin foundCheckin = null;
+        if (checkinDTO.getId() != null) {
+            foundCheckin = checkinRepo.findById(checkinDTO.getId()).orElse(null);
+        }
 
-		} else {
-			foundCheckin.setNextAssignment(checkinDTO.getNextAssignment());
-			foundCheckin.setBlockers(checkinDTO.getBlockers());
-			foundCheckin.setBlockerDescription(checkinDTO.getBlockerDescription());
-			foundCheckin.setRole(checkinDTO.getRole());
-			foundCheckin.setCodingType(checkinDTO.getCodingType());
-			foundCheckin = checkinRepo.save(foundCheckin);
-		}
-		CheckinDTO returnCheckinDTO = new CheckinDTO(foundCheckin);
+        if (foundCheckin == null) {
+            Checkin checkin = new Checkin(checkinDTO, uid);
+            setStudentFromUid(checkin, uid);
+            foundCheckin = checkinRepo.save(checkin);
 
-		return returnCheckinDTO;
-	}
+        } else {
+            foundCheckin.setNextAssignment(checkinDTO.getNextAssignment());
+            foundCheckin.setBlockers(checkinDTO.getBlockers());
+            foundCheckin.setBlockerDescription(checkinDTO.getBlockerDescription());
+            foundCheckin.setRole(checkinDTO.getRole());
+            foundCheckin.setCodingType(checkinDTO.getCodingType());
+            foundCheckin = checkinRepo.save(foundCheckin);
+        }
+        CheckinDTO returnCheckinDTO = new CheckinDTO(foundCheckin);
 
-	private void setStudentFromUid(Checkin checkin, String uid) {
-		Student student = studentRepo.findByUid(uid);
-		if (student != null) {
-			checkin.setStudent(student);
-		}
-	}
+        return returnCheckinDTO;
+    }
 
-	public List<Checkin> findAll() {
-		return checkinRepo.findAll().stream().sorted(Comparator.comparing(Checkin::getDate).reversed())
-				.collect(Collectors.toList());
-	}
+    private void setStudentFromUid(Checkin checkin, String uid) {
+        Student student = studentRepo.findByUid(uid);
+        if (student != null) {
+            checkin.setStudent(student);
+        }
+    }
 
-	public Checkin findById(Long id) {
-		return checkinRepo.findById(id).get();
-	}
+    public List<Checkin> findAll() {
+        return checkinRepo.findAll().stream().sorted(Comparator.comparing(Checkin::getDate).reversed())
+                .collect(Collectors.toList());
+    }
 
-	public void delete(CheckinDTO checkinDTO, String uid) {
-		Checkin foundCheckin = checkinRepo.findById(checkinDTO.getId()).orElse(null);
-		if (foundCheckin != null && foundCheckin.getUid().equals(uid)) {
-			checkinRepo.delete(foundCheckin);
-		}
-	}
+    public Checkin findById(Long id) {
+        return checkinRepo.findById(id).get();
+    }
 
-	public List<CheckinDTO> findByUid(String uid) {
-		List<Checkin> checkins = checkinRepo.findByUid(uid).stream()
-				.sorted(Comparator.comparing(Checkin::getDate).reversed()).collect(Collectors.toList());
-		return checkins.stream().map(CheckinDTO::new).collect(Collectors.toList());
+    public void delete(CheckinDTO checkinDTO, String uid) {
+        Checkin foundCheckin = checkinRepo.findById(checkinDTO.getId()).orElse(null);
+        if (foundCheckin != null && foundCheckin.getUid().equals(uid)) {
+            checkinRepo.delete(foundCheckin);
+        }
+    }
 
-	}
+    public List<CheckinDTO> findByUid(String uid) {
+        List<Checkin> checkins = checkinRepo.findByUid(uid).stream()
+                .sorted(Comparator.comparing(Checkin::getDate).reversed()).collect(Collectors.toList());
+        return checkins.stream().map(CheckinDTO::new).collect(Collectors.toList());
+
+    }
 }
--- src/test/java/com/coderscampus/cp/service/CheckinServiceTest.java
+++ src/test/java/com/coderscampus/cp/service/CheckinServiceTest.java
@@ -1,69 +1,66 @@
 package com.coderscampus.cp.service;
 
-import java.util.List;
-import java.util.UUID;
-
-import com.coderscampus.cp.domain.ActivityLog;
 import com.coderscampus.cp.domain.Checkin;
+import com.coderscampus.cp.domain.Student;
 import com.coderscampus.cp.dto.CheckinDTO;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
+import com.coderscampus.cp.repository.CheckinRepository;
+import com.coderscampus.cp.repository.StudentRepository;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import com.coderscampus.cp.domain.Student;
-import com.coderscampus.cp.repository.CheckinRepository;
-import com.coderscampus.cp.repository.StudentRepository;
+import java.util.List;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 public class CheckinServiceTest {
-	
-	@Autowired
-	private StudentService studentService;
-	@Autowired
-	private StudentRepository studentRepo;
-	
-	@Autowired
-	private CheckinService checkinService;
-	@Autowired
-	private CheckinRepository checkinRepo;
+
+    @Autowired
+    private StudentService studentService;
+    @Autowired
+    private StudentRepository studentRepo;
+
+    @Autowired
+    private CheckinService checkinService;
+    @Autowired
+    private CheckinRepository checkinRepo;
 
 //    Consider for later, more streamlined
 //    @BeforeEach
 //    void setUpBeforeClass() throws Exception {}
-	
-	@Test
-	void testDeleteCheckin() {
-		//Create UID
-        String uid = UUID.randomUUID().toString();
-		//Create new student with new UID
-        Student student = new Student(uid, "Bobby", 12, "IntelliJ", false, "name", null);
-		//Save the student
-        studentRepo.save(student);
-		//Create new checkin
-        Checkin checkin = new Checkin(uid, null, 9, true, "assignment9", student, Checkin.Role.CODER, Checkin.CodingType.CRUD);
-		//Save checkin
-        checkinRepo.save(checkin);
-		//Instantiate a checkin DTO from a new checkin
-        CheckinDTO checkinDTO = new CheckinDTO(checkin);
-
-		//Create second checkin object from checkin DTO
-        Checkin foundCheckin = new Checkin(checkinDTO, uid);
-		//Confirm existence of second checkin in database
-        assertTrue(checkinRepo.findById(foundCheckin.getId()).isPresent());
-		//Delete second checkin
-        checkinRepo.delete(foundCheckin);
-		//Confirm deletion from database
-        assertFalse(checkinRepo.findById(foundCheckin.getId()).isPresent());
-		//Clean up by deleting student from database
-        studentRepo.delete(student);
-	}
 
     @Test
-    void testSaveByUid(){
+    void testDeleteCheckin() {
+        //Create UID
+        String uid = UUID.randomUUID().toString();
+        //Create new student with new UID
+        Student student = new Student(uid, "Bobby", 12, "IntelliJ", false, "name", null);
+        //Save the student
+        studentRepo.save(student);
+        //Create new checkin
+        Checkin checkin = new Checkin(uid, null, 9, true, "assignment9", student, Checkin.Role.CODER, Checkin.CodingType.CRUD);
+        //Save checkin
+        checkinRepo.save(checkin);
+        //Instantiate a checkin DTO from a new checkin
+        CheckinDTO checkinDTO = new CheckinDTO(checkin);
+
+        //Create second checkin object from checkin DTO
+        Checkin foundCheckin = new Checkin(checkinDTO, uid);
+        //Confirm existence of second checkin in database
+        assertTrue(checkinRepo.findById(foundCheckin.getId()).isPresent());
+        //Delete second checkin
+        checkinRepo.delete(foundCheckin);
+        //Confirm deletion from database
+        assertFalse(checkinRepo.findById(foundCheckin.getId()).isPresent());
+        //Clean up by deleting student from database
+        studentRepo.delete(student);
+    }
+
+    @Test
+    void testSaveByUid() {
         //Create UID
         String uid = UUID.randomUUID().toString();
         //Create new student with new UID
@@ -112,35 +109,35 @@ public class CheckinServiceTest {
         //Clean up by deleting student from database
         studentRepo.delete(student);
     }
+
     @Test
-    void testFindByUid(){
-    	//Create UID
+    @Transactional
+    void testFindByUid() {
+        //Create UID
         String uid = UUID.randomUUID().toString();
         String uid2 = UUID.randomUUID().toString();
         //Create new student with new UID
         Student student = new Student(uid, "Bobby", 12, "IntelliJ", false, "name", null);
-        Student student2 = new Student(uid2, "Bobby", 12, "IntelliJ", false, "name", null);
+//        Student student2 = new Student(uid2, "Bobby", 12, "IntelliJ", false, "name", null);
         //Save the student
         studentRepo.save(student);
-        studentRepo.save(student2);
+//        studentRepo.save(student2);
         //Create new checkin
         //Please fix the checkin constructor so that an id is not required
         Checkin checkin = new Checkin(uid, null, 9, true, "assignment9", student, Checkin.Role.CODER, Checkin.CodingType.CRUD);
-        Checkin checkin2 = new Checkin(uid2, null, 9, true, "assignment10", student2, Checkin.Role.CODER, Checkin.CodingType.CRUD);
+        Checkin checkin2 = new Checkin(uid, null, 9, true, "assignment10", student, Checkin.Role.CODER, Checkin.CodingType.CRUD);
         //Save checkin
-        checkinRepo.save(checkin);
-        checkinRepo.save(checkin2);
-        System.out.println(checkin);
-        System.out.println(checkin2);
-        System.out.println(student);
-        System.out.println(student2);
-        //please note that checkins are not listed in students above
+//        checkinRepo.save(checkin);
+//        checkinRepo.save(checkin2);
+        CheckinDTO checkinDTO1 = checkinService.saveByUid(new CheckinDTO(checkin), uid);
+        CheckinDTO checkinDTO2 = checkinService.saveByUid(new CheckinDTO(checkin2), uid);
+//
         List<CheckinDTO> checkinDTOs = checkinService.findByUid(uid);
-        for(CheckinDTO checkinDTO: checkinDTOs) {
-        	assertEquals(checkin.getId(), checkinDTO.getId());
-        }
-        //start here
-        
+        assertEquals(checkin.getId(), checkinDTOs.get(0).getId());
+        assertEquals(checkin2.getId(), checkinDTOs.get(1).getId());
+        assertEquals(2, checkinDTOs.size());
+
+
     }
-    
+
 }
