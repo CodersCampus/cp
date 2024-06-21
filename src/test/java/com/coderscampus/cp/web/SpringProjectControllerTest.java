303bd8b96d4a0efedc029cac18809c4678104d78
Zack Laine <rumtehk_uh@yahoo.com>
2024-06-21 09:58:51 -0400

    Started SpringProjectControllerTest to test dash landing

---
+++ src/test/java/com/coderscampus/cp/web/SpringProjectControllerTest.java
@@ -0,0 +1,22 @@
+package com.coderscampus.cp.web;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SpringProjectControllerTest {
+
+    @Test
+    void getDashboard() {
+//        Land on root
+//        Forced google login
+//          (Maybe test for login showing?)
+//        Automatically create student in background if not existing
+//          Test if login provider gives enough detail to create user with
+//          Test if Student and StudentDTO were created
+//          Test if Student created is same as Student info provided by login provider
+//          Test to make sure server never sends back UID
+//        Implicit that page itself never shows (Load in visual-esque)
+//        Show contents of checkin/read
+    }
+}
