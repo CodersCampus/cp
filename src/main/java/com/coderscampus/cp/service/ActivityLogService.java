5567d2a7b761ac58cbc22558a56e20622469d0ac
alfuguo <137233519+alfuguo@users.noreply.github.com>
2024-09-24 14:29:51 -0400

    issue628 Implement/Create activityLog service code/methods
    update the ActivityLogService

--- src/main/java/com/coderscampus/cp/service/ActivityLogService.java
+++ src/main/java/com/coderscampus/cp/service/ActivityLogService.java
@@ -1,11 +1,39 @@
 package com.coderscampus.cp.service;
 
 import com.coderscampus.cp.domain.ActivityLog;
+import com.coderscampus.cp.domain.Checkin;
+import com.coderscampus.cp.domain.Student;
+import com.coderscampus.cp.dto.CheckinDTO;
+import com.coderscampus.cp.repository.ActivityLogRepository;
+import com.coderscampus.cp.repository.CheckinRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ActivityLogService {
-    public void saveByUid(ActivityLog activityLog, String uid) {
+    @Autowired
+    private CheckinRepository checkinRepo;
+    @Autowired
+    private StudentService studentService;
+    @Autowired
+    private GoogleUIDValidationService googleUIDValidationService;
+    @Autowired
+    private CheckinService checkinService;
+    @Autowired
+    private ActivityLogRepository activityLogRepository;
 
+    public ActivityLog save(ActivityLog activityLog) {
+        CheckinDTO checkinDTO = checkinService.findById(activityLog.getCheckin().getId());
+        if (checkinDTO != null && checkinDTO.getId() != null) {
+            return activityLogRepository.save(activityLog);
+        }
+        return null;
     }
+
+    public ActivityLog findById(Long id) {
+        ActivityLog foundActivityLog = activityLogRepository.findById(id).orElse(null);
+        return foundActivityLog;
+    }
+
+
 }
