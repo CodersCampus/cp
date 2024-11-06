e52dd9f47ed4a8c600710f63273057009924758e
OttoMaticCoder <123340921+OttoMaticCoder@users.noreply.github.com>
2024-11-06 06:56:04 -0800

    issue630 Create ActivityLog bottom right - activityLog read got activityLog to save to table

--- src/main/java/com/coderscampus/cp/web/ActivityLogController.java
+++ src/main/java/com/coderscampus/cp/web/ActivityLogController.java
@@ -2,6 +2,7 @@ package com.coderscampus.cp.web;
 
 import com.coderscampus.cp.domain.ActivityLog;
 import com.coderscampus.cp.domain.Checkin;
+import com.coderscampus.cp.repository.CheckinRepository;
 import com.coderscampus.cp.service.ActivityLogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -15,11 +16,16 @@ public class ActivityLogController {
     @Autowired
     private ActivityLogService activityLogService;
 
+    @Autowired
+    private CheckinRepository checkinRepository;
+
     @PostMapping("/create")
     public String postCreate(ActivityLog activityLog, @RequestParam("id") Long id) {
+        Checkin checkin = checkinRepository.findById(id).get();
+        activityLog.setCheckin(checkin);
         System.out.println("Here is the id" + id);
         activityLogService.save(activityLog);
         System.out.println("HEYYYYYYYYYYYYYYYYY" + activityLog);
-        return "redirect:/checkin/update/" + activityLog.getCheckin().getId();
+        return "redirect:/checkin/update/" + id;
     }
 }
