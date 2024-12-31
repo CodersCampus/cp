d2e4411140453e9ec3975c1e440998ff41cc82e3
Abigail Bales <138115235+Abagels96@users.noreply.github.com>
2024-12-30 20:00:02 -0600

    issue678 API call to Coders Campus Assignment App to auto-populate Assignment added domain object and service layer

---
+++ src/main/java/com/coderscampus/cp/domain/Assignment.java
@@ -0,0 +1,57 @@
+package com.coderscampus.cp.domain;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Assignment {
+	@JsonProperty("id")
+	private String id;
+	@JsonProperty("email")
+	private String email;
+	@JsonProperty("assignmentSubmitted")
+	private String assignmentSubmitted;
+	@JsonProperty("assignmentExpected")
+	private String assignmentExpected;
+	@JsonProperty("weeksInBootcamp")
+	private String weeksInBootcamp;
+	@JsonProperty("status")
+	private String status;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAssignmentSubmitted() {
+		return assignmentSubmitted;
+	}
+	public void setAssignmentSubmitted(String assignmentSubmitted) {
+		this.assignmentSubmitted = assignmentSubmitted;
+	}
+	public String getAssignmentExpected() {
+		return assignmentExpected;
+	}
+	public void setAssignmentExpected(String assignmentExpected) {
+		this.assignmentExpected = assignmentExpected;
+	}
+	public String getWeeksInBootcamp() {
+		return weeksInBootcamp;
+	}
+	public void setWeeksInBootcamp(String weeksInBootcamp) {
+		this.weeksInBootcamp = weeksInBootcamp;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+
+}
--- src/main/java/com/coderscampus/cp/dto/AssignmentDTO.java
+++ src/main/java/com/coderscampus/cp/dto/AssignmentDTO.java
@@ -1,10 +1,16 @@
 package com.coderscampus.cp.dto;
 
+import java.util.List;
+
+import com.coderscampus.cp.domain.Role;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class AssignmentDTO {
 	
-	@JsonProperty("cc.assignment.cp.baseUrl")
+	@JsonProperty("assignment")
 	private String assignment;
+	@JsonProperty("roles")
+	private List<Role> roles;
+	
 
 }
