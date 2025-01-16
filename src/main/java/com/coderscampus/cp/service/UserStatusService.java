e6269510147597c450d28e1c0c7cc8c069c46ac8
Jean Arnaud Tanoe <jatan60@gmail.com>
2025-01-16 01:47:37 -0600

    issue678: Used Coders Campus Assignment App API call to auto-populate Next Assignment field in GET /checkin/update view

---
+++ src/main/java/com/coderscampus/cp/service/UserStatusService.java
@@ -0,0 +1,39 @@
+package com.coderscampus.cp.service;
+
+import com.coderscampus.cp.dto.UserStatusDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.util.Objects;
+
+@Service
+public class UserStatusService {
+
+    private static final int MAX_ASSIGNMENTS = 15;
+    private final String baseUrl;
+    private final RestTemplate rt;
+
+    public UserStatusService(String baseUrl) {
+        this.baseUrl = baseUrl;
+        this.rt = new RestTemplate();
+    }
+
+    public int getUserNextAssignment(String email) {
+        int assignmentSubmitted = Integer.parseInt(Objects.requireNonNull(getUserStatus(email).getBody()).getAssignmentSubmitted());
+        return assignmentSubmitted >= MAX_ASSIGNMENTS ? assignmentSubmitted : assignmentSubmitted + 1;
+    }
+
+    public ResponseEntity<UserStatusDTO> getUserStatus(String email) {
+        URI uri = buildURI(email);
+        return rt.getForEntity(uri, UserStatusDTO.class);
+    }
+
+    public URI buildURI(String email) {
+        UriComponentsBuilder uri = UriComponentsBuilder.fromHttpUrl(baseUrl).pathSegment(email);
+        return uri.build().toUri();
+    }
+
+}
