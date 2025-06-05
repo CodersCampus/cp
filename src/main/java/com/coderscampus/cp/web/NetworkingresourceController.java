78e450e8c2c79d2511b92608eb58de0eb54e49a5
James <justjamescreative@outlook.com>
2025-06-05 14:27:11 -0400

    issue722 Networking Worksheet done

--- src/main/java/com/coderscampus/cp/web/NetworkingresourceController.java
+++ src/main/java/com/coderscampus/cp/web/NetworkingresourceController.java
@@ -10,54 +10,54 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
-@RequestMapping("/networkingresources")
+@RequestMapping("/networkingresource")
 public class NetworkingresourceController {
 
     @Autowired
-    private NetworkingresourceService networkingresourcesService;
+    private NetworkingresourceService networkingresourceService;
 
     @GetMapping("")
     public String home(ModelMap model) {
-        List<Networkingresource> networkingresources = networkingresourcesService.findAll();
-        model.put("networkingresources", networkingresources);
+        List<Networkingresource> networkingresource = networkingresourceService.findAll();
+        model.put("networkingresource", networkingresource);
         model.addAttribute("pageTitle", "Networking Resources");
-        model.put("isNetworkingresources", true);
-        return "networkingresources/read";
+        model.put("isNetworkingresource", true);
+        return "networkingresource/read";
     }
 
     @GetMapping("/create")
     public String getCreate(ModelMap model) {
-        Networkingresource networkingresources = new Networkingresource();
-        model.put("networkingresources", networkingresources);
+        Networkingresource networkingresource = new Networkingresource();
+        model.put("networkingresource", networkingresource);
         model.addAttribute("pageTitle", "Networking Resources");
-        model.put("isNetworkingresources", true);
-        return "networkingresources/create";
+        model.put("isNetworkingresource", true);
+        return "networkingresource/create";
     }
 
     @PostMapping("/create")
-    public String create(Networkingresource networkingresources, @RequestParam("uid") String uid) {
-        networkingresources = networkingresourcesService.saveByUid(networkingresources, uid);
-        return "redirect:/networkingresources";
+    public String create(Networkingresource networkingresource, @RequestParam("uid") String uid) {
+        networkingresource = networkingresourceService.saveByUid(networkingresource, uid);
+        return "redirect:/networkingresource";
     }
 
     @GetMapping("/update/{id}")
     public String fetch(ModelMap model, @PathVariable Long id) {
-        Networkingresource networkingresources = networkingresourcesService.findById(id);
-        model.put("networkingresources", networkingresources);
+        Networkingresource networkingresource = networkingresourceService.findById(id);
+        model.put("networkingresource", networkingresource);
         model.addAttribute("pageTitle", "Networking Resources");
-        model.put("isNetworkingresources", true);
-        return "networkingresources/update";
+        model.put("isNetworkingresource", true);
+        return "networkingresource/update";
     }
 
     @PostMapping("/update")
-    public String update(Networkingresource networkingresources) {
-        networkingresourcesService.save(networkingresources);
-        return "redirect:/networkingresources";
+    public String update(Networkingresource networkingresource) {
+        networkingresourceService.save(networkingresource);
+        return "redirect:/networkingresource";
     }
 
     @PostMapping("/delete")
-    public String delete(Networkingresource networkingresources) {
-        networkingresourcesService.delete(networkingresources);
-        return "redirect:/networkingresources";
+    public String delete(Networkingresource networkingresource) {
+        networkingresourceService.delete(networkingresource);
+        return "redirect:/networkingresource";
     }
 }
