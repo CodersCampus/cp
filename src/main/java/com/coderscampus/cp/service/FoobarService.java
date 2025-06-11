080d86ba1aef3de2185a7c88971a12bc7476375b
robertlassila <rlassila42@gmail.com>
2025-06-11 10:03:20 -0400

    issue815 Fix Foobar pattern to call saveByUid and remove the save method from Service layer

--- src/main/java/com/coderscampus/cp/service/FoobarService.java
+++ src/main/java/com/coderscampus/cp/service/FoobarService.java
@@ -18,10 +18,6 @@ public class FoobarService {
     @Autowired
     private StudentRepository studentRepo;
 
-    public Foobar save(Foobar foobar) {
-        return foobarRepo.save(foobar);
-    }
-
     public Foobar saveByUid(Foobar foobar, String uid) {
         if (foobar == null || uid == null) {
             return null;
