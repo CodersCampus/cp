14a5bf18c40ab31655b6fd041cbb4dd7fc901b21
TMGan <gangadeentim@gmail.com>
2025-08-13 21:00:27 -0400

    882 progress created data tables for Resume, final project etc

--- src/main/java/com/coderscampus/cp/manual/TestDataService.java
+++ src/main/java/com/coderscampus/cp/manual/TestDataService.java
@@ -1,11 +1,10 @@
 package com.coderscampus.cp.manual;
 
-import com.coderscampus.cp.domain.GitHub;
-import com.coderscampus.cp.domain.Student;
+import com.coderscampus.cp.domain.*;
 import com.coderscampus.cp.dto.CheckinDTO;
 import com.coderscampus.cp.dto.StudentDTO;
-import com.coderscampus.cp.repository.GitHubRepository;
-import com.coderscampus.cp.repository.StudentRepository;
+import com.coderscampus.cp.repository.*;
+import com.coderscampus.cp.service.FinalprojectService;
 import com.coderscampus.cp.service.GitHubService;
 import com.coderscampus.cp.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,6 +28,19 @@ public class TestDataService {
     @Autowired
     private GitHubRepository gitHubRepo;
 
+    @Autowired
+    private FinalprojectService finalprojectService;
+    @Autowired
+    private FinalprojectRepository finalprojectRepo;
+    @Autowired
+    private LinkedInRepository linkedInRepo;
+    @Autowired
+    private NetworkingpersonRepository networkingpersonRepo;
+    @Autowired
+    private NetworkingresourceRepository networkingresourceRepo;
+    @Autowired
+    private ResumeRepository resumeRepo;
+
     Student student1;
     Student student2;
 
@@ -45,13 +57,15 @@ public class TestDataService {
 
     List<GitHub> student1GitHubList;
 
+    List<Finalproject> student1FinalprojectList;
+
     void prepData() throws MalformedURLException {
 
         studentDTO1 = new StudentDTO();
         studentDTO2 = new StudentDTO();
 
-        student1Uid = UUID.randomUUID().toString();
-        student2Uid = UUID.randomUUID().toString();
+        student1Uid = "student1";
+        student2Uid = "student2";
 
         student1 = new Student(student1Uid, "name1", 1, "IntelliJ", false, "mentor1", null);
         student2 = new Student(student2Uid, "name2", 2, "IntelliJ", false, "mentor2", null);
@@ -63,23 +77,116 @@ public class TestDataService {
 
         List<StudentDTO> student1StudentDTOList = new ArrayList<StudentDTO>();
 
-        for (int i = 0; i < 4; i++) {
-            GitHub gitHub  = new GitHub();
-            gitHub.setUrl("Blocker" + i);
-            gitHub.setHandle(true);
-            gitHub.setEnhancedReadMe(true);
-            gitHub.setRenamedAssignments(true);
-            gitHub.setPinnedRepos(true);
-            gitHub.setExternalLinks("Blocker" + i);
-            gitHub.setImage(true);
-            gitHub.setHeadline(true);
-            gitHub.setImage(true);
-            gitHub.setStudent(student1);
-            gitHub.getStudent().setUid(student1Uid);
-            gitHubRepo.save(gitHub);
-            student1GitHubList.add(gitHub);
+        student1 = studentRepo.save(student1);
+        student2 = studentRepo.save(student2);
 
-            System.out.println("GitHub data created");
+
+        for (int j = 0; j < 2; j++) {
+            String student1Uid1 = student1Uid;
+            if (j == 1) {
+                student1Uid = student2Uid;
+            }
+
+            for (int i = 0; i < 4; i++) {
+                GitHub gitHub = new GitHub();
+                gitHub.setUrl("Blocker" + i);
+                gitHub.setHandle(true);
+                gitHub.setEnhancedReadMe(true);
+                gitHub.setRenamedAssignments(true);
+                gitHub.setPinnedRepos(true);
+                gitHub.setExternalLinks("Blocker" + i);
+                gitHub.setImage(true);
+                gitHub.setHeadline(true);
+                gitHub.setImage(true);
+                gitHub.setStudent(student1);
+                gitHub.getStudent().setUid(student1Uid);
+                gitHubRepo.save(gitHub);
+
+                System.out.println("GitHub data created");
+            }
+
+
+            for (int i = 0; i < 4; i++) {
+                Finalproject finalproject = new Finalproject();
+
+                finalproject.setUid(student1Uid);
+                finalproject.setProjectName("Blocker" + i);
+                finalproject.setProposal("Blocker" + i);
+                finalproject.setCrud("Blocker" + i);
+                finalproject.setTables("Blocker" + i);
+                finalproject.setViews("Blocker" + i);
+                finalproject.setStudent(student1);
+                finalproject.getStudent().setUid(student1Uid);
+                finalprojectRepo.save(finalproject);
+
+            }
+
+            for (int i = 0; i < 4; i++) {
+                LinkedIn linkedIn = new LinkedIn();
+                linkedIn.setBanner(true);
+                linkedIn.setAbout(true);
+                linkedIn.setUrl("Blocker" + i);
+                linkedIn.setFeaturedPosts(true);
+                linkedIn.setActivity(true);
+                linkedIn.setSkills(true);
+                linkedIn.setEmail(true);
+                linkedIn.setFirstName("Blocker" + i);
+                linkedIn.setLastName("Blocker" + i);
+                linkedIn.setBiography(true);
+                linkedIn.setEducation(false);
+                linkedIn.setExperience(true);
+                linkedIn.setLocation(true);
+                linkedIn.setImage(true);
+                linkedIn.setTitle(true);
+                linkedIn.setStudent(student1);
+                linkedIn.getStudent().setUid(student1Uid);
+                linkedInRepo.save(linkedIn);
+            }
+
+            for (int i = 0; i < 4; i++) {
+                Networkingperson networkingperson = new Networkingperson();
+
+                networkingperson.setUid(student1Uid);
+                networkingperson.setTechStack("Blocker" + i);
+                networkingperson.setFirstContactDate("Blocker" + i);
+                networkingperson.setLastContactDate("Blocker" + i);
+                networkingperson.setOtherNotesAboutPerson("Blocker" + i);
+                networkingperson.setStudent(student1);
+                networkingperson.getStudent().setUid(student1Uid);
+                networkingpersonRepo.save(networkingperson);
+
+                System.out.println("Networking Person data created");
+            }
+
+            for (int i = 0; i < 4; i++) {
+                Networkingresource networkingresource = new Networkingresource();
+
+                networkingresource.setUid(student1Uid);
+                networkingresource.setResourceName("Blocker" + i);
+                networkingresource.setStudent(student1);
+                networkingresource.getStudent().setUid(student1Uid);
+                networkingresourceRepo.save(networkingresource);
+
+                System.out.println("Networking resource data created");
+            }
+
+            for (int i = 0; i < 4; i++) {
+                Resume resume = new Resume();
+                resume.setPhoneNumber("Blocker" + i);
+                resume.setEmail("Blocker" + i);
+                resume.setCity("Blocker" + i);
+                resume.setState("Blocker" + i);
+                resume.setLinkedIn("Blocker" + i);
+                resume.setGitHub("Blocker" + i);
+                resume.setSummary("Blocker" + i);
+                resume.setSkills("Blocker" + i);
+                resume.setWorkExperience("Blocker" + i);
+                resume.setEducation("Blocker" + i);
+                resume.setProjects("Blocker" + i);
+                resume.setStudent(student1);
+                resume.getStudent().setUid(student1Uid);
+                resumeRepo.save(resume);
+            }
         }
     }
 }
